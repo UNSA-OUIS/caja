5f81f2625d01cda7c468a55f5ba19ddd68becc1a
Jesus Ruben Chavez <jortiz@unsa.edu.pe>
2021-03-11 20:22:47 -0500

    CRUD de tipos de concepto, añadiendo boton de atras al actualizar tipo de concepto

---
+++ app/Models/TiposConcepto.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TiposConcepto extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'tipos_concepto';
+
+    protected $fillable = ['nombre'];
+}
